b3fd56be56c4e8d6eb32590f002519dd468f8220
DrDreo <andreas.hahn@bannerflow.com>
2024-01-16 13:43:24 +0100

    format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatCacheModule } from './caches/cat-cache/cat-cache.module';
+import { UserCacheModule } from './caches/user-cache/user-cache.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [() : any => {}]
+    }),
+    CatCacheModule, UserCacheModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [CatCacheModule, UserCacheModule],
 })
 export class AppModule {}
